a53dd903766a658be05a19ac9e196c73f5ce2272
Mac Rowe <m.rowe42@gmail.com>
2021-06-12 16:44:35 -0400

    Add  click event listener to find elements, API url, and DOM Content Loaded  event listener

---
+++ index.js
@@ -0,0 +1,16 @@
+document.addEventListener("click", event => { console.log("You just clicked on:", event.target) })
+
+const API_DATABASE_URL = "http://localhost:3000/films"
+
+document.addEventListener("DOMContentLoaded", function(){ console.log("DOM Content Loaded")
+
+
+    const renderFilm = (film) =>
+
+
+
+
+
+
+
+})
