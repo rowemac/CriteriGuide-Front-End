0034645409ad957d9817c6cef7d47e9c929102eb
Mac Rowe <m.rowe42@gmail.com>
2021-06-12 17:10:10 -0400

    Add renderFilm function

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 document.addEventListener("click", event => { console.log("You just clicked on:", event.target) })
 
-const API_DATABASE_URL = "http://localhost:3000/films"
+const API_URL = "http://localhost:3000/films"
 
 document.addEventListener("DOMContentLoaded", function(){ console.log("DOM Content Loaded")
 
 
-    const renderFilm = (film) =>
+    const renderFilm = (film) => {
+        const filmCardDiv = document.createElement("div")
+        filmCardDiv.classList.add("filmCard")
+        filmCardDiv.setAttribute("data-id", film.id)
+        filmCardDiv.id = film.id
+
+        filmCardDiv.innerHTML = `
+            <img src=${film.image} class="film-image">
+            <p>${film.title} | ${film.director} | ${film.year}</p>
+            <button data-id="${film.id} class="watched-btn">Watched</button>
+            <button data-id="${film.id} class="favorite-btn">Favorite</button>
+        `
+        const filmCollectionDiv = document.querySelector("#film-collection")
+        filmCollectionDiv.append(filmCardDiv)
+    }
 
 
 
