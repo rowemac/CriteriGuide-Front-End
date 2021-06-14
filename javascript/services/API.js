2fa3f42a05098315613ed389df5dd356ab521ee9
Mac Rowe <m.rowe42@gmail.com>
2021-06-14 19:54:07 -0400

    Add spacing for readability

--- javascript/services/API.js
+++ javascript/services/API.js
@@ -1,12 +1,12 @@
 class API {
 
     static addFilms() {
-        fetch("http://localhost3000/films")
+        fetch("http://localhost:3000/films")
         .then(response => response.json())
         .then(films => {
             films.forEach( film => {
-                const {id, title, director, year, synopsis, runtime, image, favorited, watched, genre} = film
-                new Film(id, title, director, year, synopsis, runtime, image, favorited, watched, genre)
+                const { id, title, director, year, synopsis, runtime, image, favorited, watched, genre } = film
+                new Film( id, title, director, year, synopsis, runtime, image, favorited, watched, genre )
             })
         })
     }
