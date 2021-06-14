2cdbaa50b2bd4d5eb3c1b8541e2a2aaa4e668071
Mac Rowe <m.rowe42@gmail.com>
2021-06-14 19:54:49 -0400

    Comment out unused code

--- javascript/index.js
+++ javascript/index.js
@@ -4,39 +4,21 @@
 let watchedFilm = false
 let favoritedFilm = false
 
-document.addEventListener("DOMContentLoaded", function(){ console.log("DOM Content Loaded")
+document.addEventListener("DOMContentLoaded", function(){ console.log("DOM Content Loaded");
+    API.addFilms();
 
-    const renderFilm = (film) => {
-        const filmCardDiv = document.createElement("div")
-        filmCardDiv.classList.add("filmCard")
-        filmCardDiv.setAttribute("data-id", film.id)
-        filmCardDiv.id = film.id
-
-        filmCardDiv.innerHTML = `
-            <img src=${film.image} class="film-image">
-            <p class="film-info-el"><b>${film.title}</b><br>
-            <i>${film.director}</i>, ${film.year}</p>
-            <button data-id="${film.id}" class="watched-btn">Watched</button>
-            <button data-id="${film.id}" class="favorite-btn">Favorite</button>
-        `
-        const filmCollectionDiv = document.querySelector(".grid-container")
-        filmCollectionDiv.append(filmCardDiv)
-    }
-
-    const renderAllFilms = (allFilms) => {
-        allFilms.forEach( film => { renderFilm(film) } ) 
-    }
+    // const renderAllFilms = (allFilms) => {
+    //     allFilms.forEach( film => { renderFilm(film) } ) 
+    // }
 
     // fetch(API_DATABASE_URL).then(response => response.json() )
     // .then( filmsArray => { console.log(filmsArray);
     //     renderAllFilms(filmsArray) 
     // })
 
-    
+
 
     document.addEventListener("click", event => { event.preventDefault();
-        
-        
 
         if(event.target.matches(".watched-btn")) {
             console.log(event.target)
@@ -57,7 +39,7 @@ document.addEventListener("DOMContentLoaded", function(){ console.log("DOM Conte
                 watched: watchedFilm
             }
 
-            fetch(`${API_DATABASE_URL}/${id}`, {
+            fetch(`http://localhost:3000/films/${id}`, {
                 method: "PATCH",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(filmObj)
@@ -85,7 +67,7 @@ document.addEventListener("DOMContentLoaded", function(){ console.log("DOM Conte
                 favorited: favoritedFilm
             }
 
-            fetch(`${API_DATABASE_URL}/${id}`, {
+            fetch(`http://localhost:3000/films/${id}`, {
                 method: "PATCH",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(filmObj)
