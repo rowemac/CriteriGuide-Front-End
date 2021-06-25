b1d31d67f4f7af07effae4f69c0fefd3eabf0082
Mac Rowe <m.rowe42@gmail.com>
2021-06-25 08:35:53 -0400

    Update spacing for readability

--- javascript/services/API.js
+++ javascript/services/API.js
@@ -3,7 +3,7 @@ class API {
     static addFilms() {
         fetch("http://localhost:3000/films")
         .then(response => response.json())
-        .then(films => {
+        .then(films => { 
             films.forEach( film => {
                 const { id, title, director, year, synopsis, runtime, image, favorited, watched, genre } = film
                 new Film( id, title, director, year, synopsis, runtime, image, favorited, watched, genre )
